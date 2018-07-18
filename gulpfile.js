cc133aed4ab2ac94dc5b0d43d25c9f42fda66076
Aleksey <20929709+xiosky90@users.noreply.github.com>
2018-07-18 12:37:02 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -9,54 +9,52 @@ var gulp = require('gulp'),
 		ftp            = require('vinyl-ftp');
 
 // Gulp auto-reload function
-
-// gulp.task('browser-sync', function() {
-// 	browserSync({
-// 		proxy: "opencart.comn",
-// 		notify: true
-// 	});
-// });
-
-browserSync = require('browser-sync').create(),
-
 gulp.task('browser-sync', function() {
-  browserSync.init()
+	browserSync({
+		proxy: "opencart.comn",
+		notify: false
+	});
 });
+// browserSync = require('browser-sync').create(),
+// gulp.task('browser-sync', function() {
+//   browserSync.init()
+// });
 
 // Compileted stylesheet.css
 gulp.task('sass', function() {
-	return gulp.src('catalog/view/theme/**/stylesheet/stylesheet.sass')
-		.pipe(sass({
-			includePaths: bourbon.includePaths
-		}).on('error', sass.logError))
-		.pipe(autoprefixer(['last 15 versions']))
+	return gulp.src('catalog/view/theme/apple/stylesheet/stylesheet.scss')
+        .pipe(sass({
+        	includePaths: bourbon.includePaths
+        	}).on('error', sass.logError))
+		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
 		.pipe(cleanCSS())
-		.pipe(gulp.dest('catalog/view/theme/**/stylesheet/'))
+		.pipe(gulp.dest('catalog/view/theme/apple/stylesheet/'))
 		.pipe(browserSync.reload({stream: true}))
+		browserSync.reload();
 });
 
 // Watch for files gulp reload
 gulp.task('watch', ['sass', 'browser-sync'], function() {
-	gulp.watch('catalog/view/theme/**/stylesheet/stylesheet.sass', ['sass']);
-	gulp.watch('catalog/view/theme/**/template/**/*', browserSync.reload);
-	gulp.watch('catalog/view/theme/**/js/**/*.js', browserSync.reload);
-	gulp.watch('catalog/view/theme/**/libs/**/*', browserSync.reload);
+	gulp.watch('catalog/view/theme/apple/stylesheet/**.scss', ['sass']);
+	gulp.watch('catalog/view/theme/apple/template/**/*.twig', browserSync.reload);
+	gulp.watch('catalog/view/theme/apple/js/**/*.js', browserSync.reload);
+	gulp.watch('catalog/view/theme/apple/libs/**/*', browserSync.reload);
 });
 
 // Change on host files
-gulp.task('deploy', function() {
-	var conn = ftp.create({
-		host:      'hostname.com',
-		user:      'username',
-		password:  'userpassword',
-		parallel:  10,
-		log: gutil.log
-	});
-	var globs = [
-	'catalog/view/theme/apple/**'
-	];
-	return gulp.src(globs, {buffer: false})
-	.pipe(conn.dest('/path/to/folder/on/server'));
-});
+// gulp.task('deploy', function() {
+// 	var conn = ftp.create({
+// 		host:      'hostname.com',
+// 		user:      'username',
+// 		password:  'userpassword',
+// 		parallel:  10,
+// 		log: gutil.log
+// 	});
+// 	var globs = [
+// 	'catalog/view/theme/apple/**'
+// 	];
+// 	return gulp.src(globs, {buffer: false})
+// 	.pipe(conn.dest('/path/to/folder/on/server'));
+// });
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['browser-sync', 'watch']);
